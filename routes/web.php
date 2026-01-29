4b6b36bd446e99598b2c0d185acd834d7ed63549
mzhratul <159678681+mzhratul@users.noreply.github.com>
2026-01-29 22:24:31 +0600

    Remove frontend route. Group public & private route for administration

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
+
+Route::prefix('access-administration')->middleware('guest:web')->group(function () {
+
+});
+
+Route::prefix('access-administration')->middleware('auth:web')->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
